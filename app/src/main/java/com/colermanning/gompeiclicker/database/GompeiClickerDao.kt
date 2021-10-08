542d389908af716dbd7ecbc30c6279304f7fb51c
Cole Manning // RVRX <colemanning@yandex.com>
2021-10-08 00:15:46 -0400

    create DAO

---
+++ app/src/main/java/com/colermanning/gompeiclicker/database/GompeiClickerDao.kt
@@ -0,0 +1,35 @@
+package com.colermanning.gompeiclicker.database
+
+import androidx.room.Dao
+import androidx.room.Query
+import androidx.room.Update
+
+/**
+ * Database Access Object for the DB,
+ * contains functions for each database
+ * operation you want to perform
+ */
+@Dao
+interface GompeiClickerDao {
+
+    //todo, getPoints
+//    @Query("...")
+//    fun getPoints(): Int
+
+    //todo, setPoints()
+//    @Update
+//    fun setPoints()
+
+    //todo, get Upgrades...
+    //  might need to make upgrade class?
+    //  or get individual pieces of upgrades..?
+//    @Query("SELECT * FROM upgrade ")
+//    fun getUpgrades(): ...
+
+    //todo, update a specific upgrade's bought boolean
+//    @Update
+//    fun updateUpgradeBoughtStatus(boolean: Boolean) //todo, not sure about arguments...
+
+
+    //todo, more DAO functions...
+}
