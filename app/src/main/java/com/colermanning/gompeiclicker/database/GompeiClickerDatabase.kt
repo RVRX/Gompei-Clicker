1974f891c81a7f7dd2af2c06bf43426fcc385e10
Cole Manning // RVRX <colemanning@yandex.com>
2021-10-07 23:51:19 -0400

    add DB type converter for UUID

--- app/src/main/java/com/colermanning/gompeiclicker/database/GompeiClickerDatabase.kt
+++ app/src/main/java/com/colermanning/gompeiclicker/database/GompeiClickerDatabase.kt
@@ -2,6 +2,7 @@ package com.colermanning.gompeiclicker.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.colermanning.gompeiclicker.Game
 import com.colermanning.gompeiclicker.Upgrade
 
@@ -10,5 +11,6 @@ import com.colermanning.gompeiclicker.Upgrade
  * todo, p 224
  */
 @Database(entities = [ Game::class, Upgrade::class ], version = 1)
+@TypeConverters(GompeiClickerTypeConverters::class)
 abstract class GompeiClickerDatabase : RoomDatabase() {
 }
