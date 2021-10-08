a591ad762367d7d53015ce25be7baa7802228aea
Cole Manning // RVRX <colemanning@yandex.com>
2021-10-07 21:10:00 -0400

    vars for points+gompei, add click listener gompei

--- app/src/main/java/com/colermanning/gompeiclicker/ui/main/gameFragment.kt
+++ app/src/main/java/com/colermanning/gompeiclicker/ui/main/gameFragment.kt
@@ -1,13 +1,22 @@
 package com.colermanning.gompeiclicker.ui.main
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.colermanning.gompeiclicker.R
 
+private const val TAG = "gameFragment"
+
 class gameFragment : Fragment() {
+
+    //lateinits
+    private lateinit var gompeiImageView: ImageView
+    private lateinit var pointsTextView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +28,20 @@ class gameFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_game, container, false)
+
+        //get references to xml elements
+        gompeiImageView = view.findViewById(R.id.gompeiImageView)
+        pointsTextView = view.findViewById(R.id.pointValueText)
+
+
+        // Gompei Image onClick Listener
+        gompeiImageView.setOnClickListener { view: View ->
+            Log.d(TAG, "Gompei Clicked!")
+
+            //todo, click action
+            //  ...
+        }
+
 
         return view
     }
