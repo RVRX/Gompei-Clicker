1a1bf2f5f5225b2e147634956a00c808ecb5fccb
Cole Manning // RVRX <colemanning@yandex.com>
2021-10-11 14:20:57 -0400

    working shake detector

--- app/src/main/java/com/colermanning/gompeiclicker/MainActivity.kt
+++ app/src/main/java/com/colermanning/gompeiclicker/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.colermanning.gompeiclicker
 
+import android.content.Context
 import android.media.MediaPlayer
 import android.os.Bundle
 import android.util.Log
@@ -16,13 +17,24 @@ import com.colermanning.gompeiclicker.ui.main.*
 import com.colermanning.gompeiclicker.ui.main.BackgroundSoundService
 
 import android.content.Intent
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.widget.Toast
+import java.lang.Math.sqrt
+import java.util.*
 
-
-
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity(), GameStartFragment.Callbacks, SettingsFragment.Callbacks {
 
     private lateinit var binding: ActivityMainBinding
+
+    private var sensorManager: SensorManager? = null
+    private var acceleration = 0f
+    private var currentAcceleration = 0f
+    private var lastAcceleration = 0f
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -46,6 +58,42 @@ class MainActivity : AppCompatActivity(), GameStartFragment.Callbacks, SettingsF
                 .commit()
         }
 
+        //sensor
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        Objects.requireNonNull(sensorManager)!!.registerListener(sensorListener, sensorManager!!
+            .getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL)
+        acceleration = 10f
+        currentAcceleration = SensorManager.GRAVITY_EARTH
+        lastAcceleration = SensorManager.GRAVITY_EARTH
+
+    }
+
+    private val sensorListener: SensorEventListener = object : SensorEventListener {
+        override fun onSensorChanged(event: SensorEvent) {
+            val x = event.values[0]
+            val y = event.values[1]
+            val z = event.values[2]
+            lastAcceleration = currentAcceleration
+            currentAcceleration = sqrt((x * x + y * y + z * z).toDouble()).toFloat()
+            val delta: Float = currentAcceleration - lastAcceleration
+            acceleration = acceleration * 0.9f + delta
+            if (acceleration > 1) {
+                //todo, call a gompeiClick from fragment...
+                Log.e(TAG, "SHAKEY SHAKE")
+                Toast.makeText(applicationContext, "Shake event detected", Toast.LENGTH_SHORT).show()
+            }
+        }
+        override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {}
+    }
+    override fun onResume() {
+        sensorManager?.registerListener(sensorListener, sensorManager!!.getDefaultSensor(
+            Sensor .TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL
+        )
+        super.onResume()
+    }
+    override fun onPause() {
+        sensorManager!!.unregisterListener(sensorListener)
+        super.onPause()
     }
 
     override fun onStartGameSelected(){
