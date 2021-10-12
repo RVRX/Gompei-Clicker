71d1a111459aad97d07d774293b8066d98e3efde
Sam Rowe <sampatrick07@gmail.com>
2021-10-11 21:13:08 -0400

    Increased point values of most upgrades so they scale better.

--- app/src/main/java/com/colermanning/gompeiclicker/GompeiClickerRepository.kt
+++ app/src/main/java/com/colermanning/gompeiclicker/GompeiClickerRepository.kt
@@ -78,14 +78,14 @@ class GompeiClickerRepository private constructor(context: Context) {
             //Upgrade options
             addUpgrade("Grass", "ClickValue", "Gompei eats a bit of fresh grass off of the ground. All natural! Each click now earns 1.25x the amount of innovation bucks.", 20, 1.25)
             addUpgrade("Straw", "ClickValue", "Gompei eats some straw that he found! He's not quite full, but it's still satisfying enough. Each click earns 1.5x the amount of innovation bucks.", 25, 1.5)
-            addUpgrade("Sugar", "ClickValue", "Gompei eats a sugar cube! He's become a bit hyperactive, now he earns 2x the amount of innovation bucks!", 50, 2.0)
-            addUpgrade("Basic Education", "ClickValue", "Gompei receives some basic education! He's learned the basics of how to innovate, and now earns 2.5x the amount of innovation bucks!", 100,2.5)
-            addUpgrade("Hay", "ClickValue", "Gompei gets some hay to eat! He thinks it's delicious and is motivated to work harder than before! He now earns 3x points!", 500,3.0)
-            addUpgrade("Watermelon", "ClickValue", "You give Gompei some watermelon as a sweet dessert. He loves it! Each click now earns 3.5x the amount of innovation bucks.", 1000, 3.5)
-            addUpgrade("Advanced Education", "ClickValue", "Gompei gets smarter every day! New he's one of the smartest goats around! Each click now earns 4x the amount of innovation bucks.", 1500, 4.0)
-            addUpgrade("Steak", "ClickValue", "Goats are normally herbivores, but Gompei has decided to experiment a little bit. Each click now earns 5x the amount of innovation bucks.", 2500, 5.0)
-            addUpgrade("Vegetarian Buffet!", "ClickValue", "You give Gompei an assortment of the finest veggies around. He couldn't be happier! Each click now earns 6x points!", 5000, 6.0)
-            addUpgrade("Higher Education", "ClickValue", "Gompei has received a world class education at WPI. He is now a true innovator! Each click now earns 10x points!", 52320, 10.0)
+            addUpgrade("Sugar", "ClickValue", "Gompei eats a sugar cube! He's become a bit hyperactive, now he earns 2x the amount of innovation bucks!", 100, 2.0)
+            addUpgrade("Basic Education", "ClickValue", "Gompei receives some basic education! He's learned the basics of how to innovate, and now earns 2.5x the amount of innovation bucks!", 500,2.5)
+            addUpgrade("Hay", "ClickValue", "Gompei gets some hay to eat! He thinks it's delicious and is motivated to work harder than before! He now earns 3x points!", 5000,3.0)
+            addUpgrade("Watermelon", "ClickValue", "You give Gompei some watermelon as a sweet dessert. He loves it! Each click now earns 3.5x the amount of innovation bucks.", 10000, 3.5)
+            addUpgrade("Advanced Education", "ClickValue", "Gompei gets smarter every day! New he's one of the smartest goats around! Each click now earns 4x the amount of innovation bucks.", 30000, 4.0)
+            addUpgrade("Steak", "ClickValue", "Goats are normally herbivores, but Gompei has decided to experiment a little bit. Each click now earns 5x the amount of innovation bucks.", 50000, 5.0)
+            addUpgrade("Vegetarian Buffet!", "ClickValue", "You give Gompei an assortment of the finest veggies around. He couldn't be happier! Each click now earns 6x points!", 100000, 6.0)
+            addUpgrade("Higher Education", "ClickValue", "Gompei has received a world class education at WPI. He is now a true innovator! Each click now earns 10x points!", 523200, 10.0)
         }
     }
 
